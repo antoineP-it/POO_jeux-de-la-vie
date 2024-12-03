5463a74d52bceaff810273dd5a19c1be9026f7cc
antoineP-it <antoine.pouzoulet@viacesi.fr>
2024-12-03 14:46:07 +0100

    Add files via upload
    Ajout de la classe abstraite

---
+++ ABSjeux.h
@@ -0,0 +1,15 @@
+#ifndef ABSjeux
+#define ABSjeux
+
+class ABSjeux {
+    public : 
+
+    virtual void mode() =0;
+    virtual void initialiser() =0;
+    virtual void executer() =0;
+    virtual void afficher() =0;
+};
+
+
+
+#endif
