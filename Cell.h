fce2a9bb414d8a71278b714c0ba2fc58a364258b
AnasB27 <anas.bazi@viacesi.fr>
2024-12-03 11:01:08 +0100

    Add files via upload

---
+++ Cell.h
@@ -0,0 +1,36 @@
+#ifndef CELL_H
+#define CELL_H
+
+#include <Grille.h> //Héritage
+
+
+class Cell {
+    protected:
+        bool estVivant;
+        bool estObstacle;
+        int x;
+        int y;
+    
+    public:
+        Cell (); //Constructeur par défaut
+        Cell(bool vivant, bool obstacle, int coordx, int coordy); //Constructeur paramétré
+
+        //Accesseurs
+        bool getEstVivant() const;
+        bool getEstObstacle() const; 
+        int getCoordX() const;
+        int getCoordY() const;
+
+        //Modificateur
+        void setEstVivant(bool etat);
+        void setEstObstacle(bool obs);
+        void setCoordX(int coordx);
+        void setCoordY(int coordy);
+
+        //Méthodes
+        void inverseEtat(); //Méthode permettant d'inverser l'état de vivant à mort et inversement
+
+
+
+};
+#endif
