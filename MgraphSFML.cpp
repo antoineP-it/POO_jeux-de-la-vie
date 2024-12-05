bcae714247e0e18cf9bca7261fcb357a2033093b
AnasB27 <anas.bazi@viacesi.fr>
2024-12-05 11:21:40 +0100

    Update MgraphSFML.cpp

--- MgraphSFML.cpp
+++ MgraphSFML.cpp
@@ -2,11 +2,11 @@
 
 
 MgraphSFML::MgraphSFML(int width, int height, const string& title)
-    : window(sf::VideoMode(width, height), title) {}
+    : fenetre(sf::VideoMode(width, height), title) {}
 
 
 void MgraphSFML::creeFenetre() {
-    window.clear(sf::Color::Black); // pour effacer le contenu précédent de la fenêtre avec un fond noir.
+    fenetre.clear(sf::Color::Black); // pour effacer le contenu précédent de la fenêtre avec un fond noir.
 }
 
 
@@ -15,13 +15,13 @@ void MgraphSFML::renduCell(int x, int y, sf::Color color) { //Méthode pour dess
     sf::RectangleShape cell(sf::Vector2f(10.0f, 10.0f)); // on définit ici la cellule comme un rectangle
     cell.setPosition(static_cast<float>(x * 10), static_cast<float>(y * 10));
     cell.setFillColor(color);
-    window.draw(cell);
+    fenetre.draw(cell);
 }
 
 
 void MgraphSFML::handleEvents() {//méthode pour gérer les événements utilisateur
     sf::Event event;
-    while (window.pollEvent(event)) {
+    while (fenetre.pollEvent(event)) {
         if (event.type == sf::Event::Closed) {
             closeFenetre(); // Ferme la fenêtre si l'utilisateur clique sur la croix
         }
@@ -30,15 +30,15 @@ void MgraphSFML::handleEvents() {//méthode pour gérer les événements utilisa
 
 
 void MgraphSFML::closeFenetre() { // fermeture de la fenêtre
-    window.close();
+    fenetre.close();
 }
 
 
 void MgraphSFML::displayFenetre() {// on affiche le contenu de la fenêtre
-    window.display();
+    fenetre.display();
 }
 
 
 bool MgraphSFML::estOuverte() const {// on  vérifie si la fenêtre est ouverte
-    return window.isOpen();
+    return fenetre.isOpen();
 }
