a9946935ab773cc69bd79b13406c210786dcfc5d
antoineP-it <antoine.pouzoulet@viacesi.fr>
2024-12-06 11:59:00 +0100

    Update MgraphSFML.h

--- MgraphSFML.h
+++ MgraphSFML.h
@@ -3,26 +3,29 @@
 
 #include <SFML/Graphics.hpp>
 #include <string>
+#include "Grille.h"
 
 using namespace std;
+using namespace sf;
 
 class MgraphSFML {
 private:
-    sf::RenduFenetre fenetre; // on utilise cet attribut pour  la fenêtre graphique où tout sera dessiné
+    sf::RenderWindow fenetre; // on utilise cet attribut pour  la fenêtre graphique où tout sera dessiné
+    int largeur, longueur;
 
 public:
 
-    MgraphSFML(int width = 800, int height = 600, const string& title = "Jeu de la Vie");
-
+    MgraphSFML(int largeur, int longueur, const string& title);
 
     void creeFenetre();                      // Préparation la fenêtre
     void renduCell(int x, int y, sf::Color color); // on dessine une cellule
     void handleEvents();                     // Gestionles événements utilisateur
     void closeFenetre();                     // Fermeture la fenêtre
     void displayFenetre();                   // on affiche le contenu de la fenêtre
-
+    void renduGrille (const Grille& grille);
 
     bool estOuverte() const;// on vérifie si la fenêtre est ouverte
 };
+
 
 #endif
