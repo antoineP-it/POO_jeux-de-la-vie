8e677e794d9c9ad6ec7fe8885fca3222f1eefcd8
antoineP-it <antoine.pouzoulet@viacesi.fr>
2024-12-06 09:38:52 +0100

    Update GestionFichier.cpp

--- GestionFichier.cpp
+++ GestionFichier.cpp
@@ -10,25 +10,43 @@ GestionFichier::GestionFichier(){
 }
 
 
-void GestionFichier::ImportGrille() {
+std::vector<int> GestionFichier::dimensionsGrille() {
     int longueur;
     int largeur;
-    std::vector<std::vector<int>> grille;
-
+    
     std::ifstream fichier( this -> cheminAcces ); 
     if (!fichier) {
         std::cout << "Erreur : Impossible d'ouvrir le fichier " << this -> cheminAcces << std::endl;
-        return;
     }
 
     std::string ligne;
+
     if (std::getline(fichier, ligne)) {
         std::istringstream flux(ligne);
         flux >> longueur >> largeur;
     } else {
         std::cout << "Erreur : Fichier vide ou format incorrect." << std::endl;
-        return;
+
     }
+    fichier.close(); 
+    std::vector<int> dim;
+    dim.push_back(longueur);
+    dim.push_back(largeur);
+    return dim;
+}
+
+std::vector<std::vector<int>> GestionFichier::contenueGrille() {
+    std::vector<std::vector<int>> grille;
+
+    std::ifstream fichier(this->cheminAcces);
+    if (!fichier) {
+        std::cerr << "Erreur : Impossible d'ouvrir le fichier " << this->cheminAcces << std::endl;
+        return grille;
+    }
+
+    std::string ligne;
+    // Ignorer la première ligne contenant les dimensions
+    std::getline(fichier, ligne);
 
     while (std::getline(fichier, ligne)) {
         std::istringstream flux(ligne);
@@ -39,10 +57,10 @@ void GestionFichier::ImportGrille() {
             ligneGrille.push_back(valeur);
         }
 
-        grille.push_back(ligneGrille); 
+        grille.push_back(ligneGrille);
     }
-    
-    fichier.close(); 
-}
 
+    fichier.close();
+    return grille;
+}
 
