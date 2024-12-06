a5498a22268f36c5c7fbb2c227a8a82a9b7f0123
AnasB27 <anas.bazi@viacesi.fr>
2024-12-06 10:05:33 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,60 @@
 #include "Grille.h"
+#include "MGraphSFML.h"
+#include "Mconsole.h"
+#include <iostream>
+
+void Menu() {
+    std::cout << "=== Jeu de la vie ==="<< std::endl;
+    std::cout << "1. Mode Graphique!"<<std::endl;
+    std::cout <<"2. Mode Console!"<<std::endl;
+    std::cout <<"Choisissez votre option : ";
+}
 
 int main(){
-    GestionFichier fichier;
-    Grille grille(fichier.dimensionsGrille()[1], fichier.dimensionsGrille()[0]);
-    //grille.initialiseRandom(); //à changer afin de l'initialiser en fonction du fichier source 
-    auto contenu = fichier.contenueGrille();
-    grille.initialiseDepuisFichier(contenu);
+    const int largeur=80; //Dimensions de la grille
+    const int longueur=60;
 
-    grille.afficher();
-    for (int i=0; i<5; i++){
-        grille.miseAJour();
-        grille.afficher();
-    }
+    Grille grille(largeur, longueur);
+    grille.initialiseRandom();
 
-    return 0;
+    int choix = 0;
+    do {
+        Menu();
+        std::cin >> choix;
 
+        switch (choix) {
+            case 1: {
+                MGraphSFML moteurGraphique(largeur, longueur); //Mode graphique
 
+                while (moteurGraphique.isOpen()) {
+                    moteurGraphique.handleEvents();
+                    grille.miseAJour();
+                    moteurGraphique.renduGrille(grille);
 
-    
+                }
+                break;
+            }
+            case 2: {
+                Mconsole moteurConsole; //Mode console
+
+                char continuer = 'y';
+                do {
+                    moteurConsole.affichage(grille);
+                    grille.miseAJour();
+
+                    std::cout << "Continuer? (y/n): ";
+                    std::cin >> continuer;
+
+                }while (continuer == 'y');
+                break;
+            }
+            case 3:
+                std::cout << "Merci d'avoir joué à notre jeu ! " << std::endl;
+                break;
+            default:
+                std::cout << "Option invalide. Veuillez réessayer" <<std::endl;
+        }
+    }while (choix !=3);
+
+    return 0
 }
